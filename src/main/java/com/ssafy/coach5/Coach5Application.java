48603acc52cf4a1b7b22b54ee726cea2991382c6
rudus1012 <rudus1012@naver.com>
2021-07-14 21:54:31 +0900

    feat: 🎸 2장 코드

---
+++ src/main/java/com/ssafy/coach5/Coach5Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.coach5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coach5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coach5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/coach5/Coach5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.coach5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coach5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
