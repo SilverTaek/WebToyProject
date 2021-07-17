9c1c8c4e07a84c5fcfbf0f6523727815f51fd2e5
rudus1012 <rudus1012@naver.com>
2021-07-17 22:14:03 +0900

    feat: 🎸 posts create & test code

--- src/main/java/com/ssafy/coach5/domain/posts/Posts.java
+++ src/main/java/com/ssafy/coach5/domain/posts/Posts.java
@@ -3,6 +3,7 @@ package com.ssafy.coach5.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -12,7 +13,7 @@ import javax.persistence.*;
 public class Posts {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 500, nullable = false)
