46c39a7be75c7f508d0f389c3b1a4006e6234120
rudus1012 <rudus1012@naver.com>
2021-07-25 14:46:38 +0900

    feat: 🎸 Posts 등록하기 기능 구현 및 테스트코드 작성

--- src/main/java/com/ssafy/coach5/domain/posts/Posts.java
+++ src/main/java/com/ssafy/coach5/domain/posts/Posts.java
@@ -17,14 +17,14 @@ public class Posts {
 
     @Column(length = 500, nullable = false)
     private String title;
-
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
     private String author;
 
     @Builder
-    public Posts(String title, String content, String author) {
+    public Posts(Long id, String title, String content, String author) {
+        this.id = id;
         this.title = title;
         this.content = content;
         this.author = author;
--- src/main/java/com/ssafy/coach5/domain/posts/PostsRepository.java
+++ src/main/java/com/ssafy/coach5/domain/posts/PostsRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.coach5.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
