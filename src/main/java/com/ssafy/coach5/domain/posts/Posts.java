d968f64fb7373c33b6185a03d8431ffc8908b42a
rudus1012 <rudus1012@naver.com>
2021-07-28 21:24:42 +0900

    refactor: 💡 커밋 확인 후 수정
    @Repository와 Posts Builder부분 ID 제거하였습니다.

--- src/main/java/com/ssafy/coach5/domain/posts/Posts.java
+++ src/main/java/com/ssafy/coach5/domain/posts/Posts.java
@@ -23,8 +23,7 @@ public class Posts {
     private String author;
 
     @Builder
-    public Posts(Long id, String title, String content, String author) {
-        this.id = id;
+    public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
